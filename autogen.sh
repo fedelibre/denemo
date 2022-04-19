e86e4a14e14c5c9f4e7320d524232c2807b41b78
Federico Bruni <fede@inventati.org>
2022-04-19 15:51:27 +0200

    Migrate from Intltool to Gettext
    The how and why is well described by this tutorial:
    https://wiki.gnome.org/MigratingFromIntltoolToGettext

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,5 @@
 aclocal $ACLOCAL_FLAGS -I build -I /usr/share/aclocal && \
 autoheader && \
 libtoolize --force && \
-intltoolize --copy --force --automake && \
 automake --add-missing --gnu && \
 autoconf
